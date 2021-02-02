cdbd2402150dddfe83ad903eaa6f208ae52a1a5b
roukell <62136980+roukell@users.noreply.github.com>
2021-02-02 22:28:53 +1100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
+import React from 'react';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hello world</p>
-      </header>
+      <Home />
     </div>
   );
 }
-
 export default App;
---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    Hello World! This is home page
+  </>
+);
+
+export default Home;
