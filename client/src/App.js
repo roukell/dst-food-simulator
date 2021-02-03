913bfc8319c4d82271db78d9719b3a5b8a757112
roukell <62136980+roukell@users.noreply.github.com>
2021-02-03 23:31:54 +1100

    updated scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Home from './pages/Home';
+import './sass/main.scss';
 
 function App() {
   return (
     <div className="App">
       <Home />
+
+      <p> Testing new background</p>
     </div>
   );
 }
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Home = () => (
-  <>
+  <div className="Home">
     Hello World! This is home page
-  </>
+  </div>
 );
 
 export default Home;
